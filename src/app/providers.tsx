146ac340baf1e5937528a5ec920bab2179f980cb
Wais Almakaleh <wais.almakaleh@gmail.com>
2025-05-26 14:15:45 -0400

    chore: remove TODO comment from providers

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { ThemeProvider } from "next-themes";
-import { SafariThemeManager } from "./safari-theme-manager"; // Adjust import path as needed
+import { SafariThemeManager } from "./safari-theme-manager";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
